6afae692816df20a25a960ad6ab927de80f75c06
Kuollivo64 <44689881+kuollivo64@users.noreply.github.com>
2022-04-19 17:42:08 -0400

    asd

--- app/src/main/java/com/example/clase18/MainActivity.java
+++ app/src/main/java/com/example/clase18/MainActivity.java
@@ -7,52 +7,85 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
 
     private TextView txtResultado;
     private EditText etPalabra;
-    private Button btnInvertir,btnMensaje;
+    private Button btnInvertir, btnMensaje, btnContVocales;
 
     private String palabra;
+    private String invertido;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         inicializarVistas();
-        btnInvertir.setOnClickListener(new View.OnClickListener()
-        {
+        btnInvertir.setOnClickListener(new View.OnClickListener() {
+            //este clic solo es reconocido por el boton que lo esta implementando este onclick solo existe para btnInvertir
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
                 obtenerInformacion();
                 invertirPalabra();
             }
         });
+        //lambda functions
+        /*btnMensaje.setOnClickListener(view -> {
+            obtenerInformacion();
+            invertirPalabra();
+        });*/
+        btnMensaje.setOnClickListener(view -> mostrarMensaje());
+        btnContVocales.setOnClickListener(view -> {
+            obtenerInformacion();
+            contarVocales();
+        });
     }
-    private void inicializarVistas()
-    {
+
+    private void inicializarVistas() {
         txtResultado = findViewById(R.id.txtResultado);
         etPalabra = findViewById(R.id.etPalabra);
         btnInvertir = findViewById(R.id.btnInvertir);
         btnMensaje = findViewById(R.id.btnMensaje);
+        btnContVocales = findViewById(R.id.btnContVocales);
     }
-    private void obtenerInformacion()
-    {
-        palabra = etPalabra.getText().toString();
+
+    private void obtenerInformacion() {
+        palabra = etPalabra.getText().toString().toLowerCase();
     }
-    private void invertirPalabra()
-    {
-        String invertido="";
+
+    private void invertirPalabra() {
+        String invertido = "";
 
         //palabra.charAt(0);
-        for (int i = palabra.length() -1; i >=0 ; i--)
-        {
+        for (int i = palabra.length() - 1; i >= 0; i--) {
             invertido = invertido + palabra.charAt(i);
         }
         txtResultado.setText(invertido);
+
+    }
+
+    private String evaluarPalabra() {
+        String mensaje = "No es Palindromo";
+        if (palabra.equals(invertido)) {
+            mensaje = "Es palindromo";
+        }
+        return mensaje;
+    }
+
+    private void mostrarMensaje() {
+        Toast.makeText(this, evaluarPalabra(), Toast.LENGTH_LONG).show();
+    }
+
+    private void contarVocales()
+    {
+        int contador=0;
+        for(int x=0;x<palabra.length();x++) {
+            if ((palabra.charAt(x)=='a') || (palabra.charAt(x)=='e') || (palabra.charAt(x)=='i') || (palabra.charAt(x)=='o') || (palabra.charAt(x)=='u')){
+                contador++;
+            }
+        }
+        txtResultado.setText(contador);
     }
 }
